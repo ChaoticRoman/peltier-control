ad7e6a49c560c320528fd7823c98f8dbbbdf6d7f
Roman Pavelka <roman.pavelka.asi@gmail.com>
2022-02-07 19:11:55 +0100

    Implement PWM output

--- main.c
+++ main.c
@@ -16,6 +16,9 @@ volatile uint8_t joystick_ADC_status = 0;
 volatile uint8_t joyX = 0;
 volatile uint8_t joyY = 0;
 
+#define LED_OUTPUT sbi(DDRB, 5)
+#define LED_ON sbi(PORTB, 5)
+#define LED_OFF cbi(PORTB, 5)
 
 #define setADC6 cbi(ADMUX, 0)
 #define setADC7 sbi(ADMUX, 0)
@@ -48,7 +51,10 @@ ISR(ADC_vect) {
 int main(void)
 {
     set_therm(7);
+
     InitLCD(0);
+
+    // LED_OUTPUT;
 
     // AVCC as a ref, left adjsuted (for 8 bit), ADC6, datasheet p.274
     // ADC6 / ADC7 setting is then switched by last bit off/on
@@ -58,20 +64,34 @@ int main(void)
     // datasheet p. 275
     ADCSRA = 0b11001111;
 
-    uint32_t tick = 0;
+    ICR1 = 100;
+    OCR1A = 0;
+    sbi(DDRB, 1);
+    TCCR1A = 0b11000000;
+    TCCR1B = 0b00010001;
 
-    //sei();
-    cli();
+    uint32_t tick = 0;
+    uint16_t temp = 0;
+    uint16_t seconds = 0;
+
+    sei();
     while(1)
     {
         if (++tick > 99999) tick = 0;
 
-        uint16_t temp = DS_get();
+/*        if (tick % 20 == 0)  // every second ca, TODO timer
+        {*/
+            temp = DS_get();
+            ++seconds;
+/*        }*/
+
+        // LCD update takes few ms, (2.5 ms last)
+        // LED_ON;
 
         LCDClear();
         LCDHome();
 
-        LCDWriteInt(tick, 5);
+        LCDWriteInt(seconds, 5);
 
         LCDChar(' ');
         LCDChar('X');
@@ -83,12 +103,24 @@ int main(void)
 
         LCDGotoXY(0, 1);
         LCDChar('T');
-        LCDWriteInt(temp >> 8, 3);
+        LCDWriteInt(temp >> 8, 2);
         LCDChar('.');
-        LCDWriteInt(temp & 0x00ff, 3);
+        LCDWriteInt(temp & 0x00ff, 2);
         LCDChar('C');
 
-        _delay_ms(1000);
+        LCDChar(' ');
+        LCDChar('P');
+        LCDWriteInt(OCR1A, 3);
+        LCDChar('%');
+
+        if (joyY >> 5 > 5 && OCR1A < 96)
+            OCR1A += 5;
+        else if (joyY >> 5 < 2 && OCR1A > 4)
+            OCR1A -= 5;
+
+        // LED_OFF;
+
+         _delay_ms(50);
     }
 
 
